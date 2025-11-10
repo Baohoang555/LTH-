34111549fc2a2740cf76a69005770fdc59718e7c
Baohoang555 <hoangbaovu4@gmail.com>
2025-11-10 16:11:03 +0700

    Delete Connect-Four-main directory

--- Connect-Four-main/Connect-Four-main/app/Server/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Network.Server (runServer)
-
-main :: IO ()
-main = do
-  putStrLn "=== CONNECT FOUR SERVER ==="
-  runServer "3000"
--- Connect-Four-main/Connect-Four-main/src/Utils/Concurrency.hs
+++
@@ -1,7 +0,0 @@
-module Utils.Concurrency (withGameLock) where
-
-import Control.Concurrent (MVar, modifyMVar)
-
--- Helper để lock và modify MVar an toàn
-withGameLock :: MVar a -> (a -> IO (a, b)) -> IO b
-withGameLock = modifyMVar
